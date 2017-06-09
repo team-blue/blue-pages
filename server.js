5c31a6a2cd1c79b31a491400d3d9307460525a1c
jeffery shivers <jefferyshivers@gmail.com>
2017-06-09 13:11:59 -0400

    test again

--- server.js
+++ server.js
@@ -17,7 +17,3 @@ app.get('/', function(req, res) {
 	// ejs render automatically looks in the views folder
 	res.render('index');
 });
-
-app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
-});
